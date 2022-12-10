6f80b0b45897872331dbc30ccf75bd84b03fbc9c
lk0103 <99809949+lk0103@users.noreply.github.com>
2022-12-10 16:43:38 +0100

    #51 #52

--- models/data_processing/data_processing.py
+++ models/data_processing/data_processing.py
@@ -1,55 +1,84 @@
 
+from PySide6.QtCore import Signal, QObject
+
+from measurementSettings import measurementSettings
+import os
+
+
 #zistit co presne sa vykresluje v grafe - ci na y je intenzita
 # a na x je len cas
 
 #skontrolovat ci je dobre ten graf
 #spytat sa ze ako zistim presne z teh tabulkz mriezky spravne udaje
-#od martina zistit ake udaje mam posielat loggerovi
+#od martina zistit ake udaje mam posielat loggerovi  ---- a
 #ako osetrit chyby - vyhadzovat errory?? poslat spravu loggeru??
 #napriklad ako v loadMeasurement
+##ci maju ist vsetky subory merania do saved_measurements
+##ze ako sa da spravit to aby sa tie naloadovane
+#data v hlavicke dali do formularu
 
-class DataProcessing:
+#spravit error vlastny - message - v measurementcontroler sa to bude logovat
+#navrh dokoncit
+
+
+class DataProcessing(QObject):
+    setSettings_ = Signal(measurementSettings)
 
     def __init__(self):
+        super(DataProcessing, self).__init__()
         self.fileName = ""
-        self.beginingOfData = "ALFA[A°]\tVLNOVÁ DĹŽKA[A]\tINTENZITA[mV]\n"
+        self.path = self.getDefaultPath()
+        self.beginingOfData = '{: <20s}\t{: <20s}\t{}\n'.format("ALFA[A°]", "VLNOVÁ DĹŽKA[A]", "INTENZITA[mV]")
+            #"ALFA[A°]\t\t\tVLNOVÁ DĹŽKA[A]\t\t\tINTENZITA[mV]\n"
+        self.settings = None
+        self.setSettings_.connect(lambda settings: self.setSettings(settings))
 
-    def createNewFile(self, settings):
-        if not settings.checkLegend():
+    def getDefaultPath(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        dirs = path.split("\\")
+        rootName = "spektra"
+        targetDir = "\\saved_measurements\\"
+        indexRoot = len(dirs) - dirs[::-1].index(rootName) - 1
+        return "\\".join(dirs[:indexRoot + 1]) + targetDir
+
+    def setSettings(self, settings):
+        self.settings = settings
+
+    def createNewFile(self):
+        if not self.settings.checkLegend():
             return False
 
-        if not self.writeLegend(settings):
+        if not self.writeLegend():
             return False
 
-        settings.storeLastLegend()
-
+        self.settings.storeLastLegend()
         return True
 
-    #NEDOKONCENE
-    def addMeasurement(self, intensity):
+
+    def addMeasurement(self, angle, waveLength, intensity):
         if self.fileName == "":
             return False
-        with open(self.fileName, 'a', encoding="utf-8") as currentFile:
-            #blablabla
-            pass
+        with open(self.path + self.fileName, 'a', encoding="utf-8") as currentFile:
+            line = '\n{: <20s}\t{: <20s}\t{}'.format(str(angle), str(waveLength), str(intensity))
+            currentFile.write(line)
         return True
 
-    def writeLegend(self, settings):
+    def writeLegend(self):
         if self.fileName == "":
             return False
 
-        with open(self.fileName, 'w', encoding="utf-8") as currentFile:
-            currentFile.write(str(settings) + "\n\n")
+        with open(self.path + self.fileName, 'w', encoding="utf-8") as currentFile:
+            currentFile.write(str(self.settings) + "\n\n")
             currentFile.write(self.beginingOfData)
         return True
 
     def setFileName(self, name):
-        self.fileName += name + ".txt"
+        self.fileName = name + ".txt"
 
     def setFilePath(self, path):
         if path[-1:] != "\\":
             path += "\\"
-        self.fileName = path + self.fileName
+        self.path = path
 
     def loadMeasurements(self, fileName):
 
@@ -63,10 +92,15 @@ class DataProcessing:
                 return measurements
 
             measurementLine = f.readline()
+            while measurementLine == "\n":
+                measurementLine = f.readline()
+
             while measurementLine != '':
                 # treba osetrit to ak subor nie je v danom formate
                 try:
                     alfa, waveLength, intensity = measurementLine.split("\t")
+                    waveLength = waveLength.replace(" ", "")
+                    intensity = intensity.replace(" ", "")
                     measurements.append([float(waveLength),
                                          float(intensity)])
                 except:
@@ -75,39 +109,3 @@ class DataProcessing:
 
         return measurements
 
-
-
-
-
-
-####    TESTY
-'''
-    dp = DataProcessing()
-    dp.setFileName("C:\\Users\\lucin\\OneDrive\\Desktop\\spektra\\spektra\\mojPokusOHlavicku.txt")
-    dp.setNameSample("utorkajsia vzorka")
-    dp.setNoteToTechnology("blabla")
-    dp.setThickness("11.3")
-    dp.setReference("ref")
-    dp.setTemperature("15.4")
-    dp.setNameDispersingElement("M465645")
-    dp.setInputCreviceWidth("3,4")
-    dp.setInputCreviceHeight("2,4")
-    dp.setOutputCreviceWidth("5,3")
-    dp.setOutputCreviceHeight("3,4")
-    dp.setOpticalFilter("filter")
-    dp.setTypeDetector("Si-fotodioda")
-    dp.setAdditionalInfoDetector("nazov")
-    dp.setTypeOfLight("Laser")
-    dp.setNameOfLight("nadupany")
-    dp.setStartAngstrom("340")
-    dp.setEndAngstrom("200")
-    dp.setStepOfMotor("1.3")
-    dp.setNumberOfMitigations("1")
-    dp.setCorrection("2")
-    dp.setLockIn("Lockin nano voltmeter type 232")
-    dp.setLockInReference("1.4")
-    dp.setLockInFilter("signal filters")
-    dp.setPhaseShift("1")
-    dp.setTimeConstant("1")
-    dp.createNewFile()
-    '''
