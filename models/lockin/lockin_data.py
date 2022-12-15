98a1339f074ef237c96e6e5bdf2194fe8fe908e0
Nesciuse <36797145+Nesciuse@users.noreply.github.com>
2022-12-15 12:14:23 +0100

    Add files via upload

---
+++ models/lockin/lockin_data.py
@@ -0,0 +1,40 @@
+import serial
+
+class SerialConnectionData:
+    def __init__(self, prt, bdr, bsz, par, stb, tim):
+        self.port = prt
+        self.baudrate = bdr
+        self.bytesize = bsz
+        self.parity = par
+        self.stopbits = stb
+        self.timeout = tim
+
+    def getsettings(self):
+        return self.port, self.baudrate, self.bytesize, self.parity, self.stopbits
+sc = SerialConnectionData
+
+lockin_data = {
+    '_template' : {
+        'gain' : [],
+        'pre_time_const' : [],
+        'post_time_const' : [],
+        'serial_connection' : None #or sc(,,,,,,)
+    }
+}
+
+with open('lockin_data.txt') as file:
+    try:
+        for line in file:
+            name = line.strip()
+            gain, pretc, postc, sercon = (eval(next(file)) for _ in range(4))
+            
+            lockin_data[name] = {
+                'gain' : gain,
+                'pre_time_const' : pretc,
+                'post_time_const' : postc,
+                'serial_connection' : None if sercon is None else sc(*sercon)
+            }
+    except Exception as e:
+        print(e)
+        print('wrong lockin_data.txt fornat')
+        
