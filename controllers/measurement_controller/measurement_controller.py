ed3e256aec8861035bfcef937c9d0ae48143f00a
Nesciuse <36797145+Nesciuse@users.noreply.github.com>
2023-01-20 13:22:48 +0100

    -newest measurement_controller

--- controllers/measurement_controller/measurement_controller.py
+++ controllers/measurement_controller/measurement_controller.py
@@ -1,154 +1,158 @@
 from PySide6 import QtCore
 from PySide6.QtCore import QObject, Signal, QThread
 
-from models.data_processing.data_processing import DataProcessing
-from PySide6.QtCore import QEventLoop
+#from models.data_processing.data_processing import DataProcessing
+from models.motor.Motor import Motor
+from models.lockin.lockin import SR510
+from models.disp_elem import Grating
+from models.data_processing.grid import Grid465645
 
+from PySide6.QtCore import QEventLoop
+import time
+import serial
 
 class MeasurementController(QObject):
     state_s = Signal(str)
     progress_s = Signal(float)
-    _motor = None
-    _lockin = None
-    _elem = None
-
+    
     def __init__(self):
         super(MeasurementController, self).__init__()
-        
+        self.angle = None
         self.running = False
-
-        self.position = None
-        disperse_element_name = 'ms732'  # z gui
-
-        print('Dostupne COM porty:')
-        availableComPorts = serial.tools.list_ports.comports()
-        for acp in availableComPorts:
-            print(acp.name + '/' + acp.description)
-
-        if self._lockin is None:
-            self._lockin = lockin.SR510()
-
-        if self._motor is None:
-            self._motor = motor.Motor('COM4')
-
-        if self._elem is None:
-            Measurement._elem = Grating(disperse_element_name)
-            if self._elem.IsCalib() is False:
-                print('kal nexist. treba kalibrovat')
-        
         self._lockin = None
         self._motor = None
-        self._data_processing = DataProcessing()
+        self._elem = None
+
+        print('Dostupne COM porty:')
+        #availableComPorts = serial.tools.list_ports.comports()
+        #for acp in availableComPorts:
+        #    print(acp.name + '/' + acp.description)
+
+        if self._lockin is None:
+            self._lockin = SR510()
+
+        if self._motor is None:
+            self._motor = Motor('COM4')
+                
+        #self._data_processing = DataProcessing()
         self._measurement_thread = QThread()
         self._measurement_thread.setPriority(QThread.HighestPriority)
         self._measurement_thread.finished.connect(self._measurement_thread.deleteLater)
         self.moveToThread(self._measurement_thread)
         self._measurement_thread.start()
 
-    @QtCore.Slot()
-    def start(self):
+    def set_dataproc_ref(self, dp, gr):
+        self.dataproc = dp
+        self.graph = gr
 
+    #@QtCore.Slot(str)
+    def disp_elem_change(self, name):
+        print(name)
+        self._elem = Grating(name)
+        if self._elem.IsCalib() is False:
+            print('kal nexist. treba kalibrovat')
+
+    def sendMeasurement(self, angle, value):
+        wavelength = Grid465645().get_wave_length(angle)
         
+        self.dataproc.data_processing.add_measurement(angle, wavelength, value)
+        self.graph.addMeasurement([[wavelength, value]], True)
+        self.graph.plotGraph()
+
+    # TODO: implement method to handle measurement from start to finish
+    # method should report progress
+    # method should be able to be stopped, therefore it should check event loop for requests during execution    
+    @QtCore.Slot(float, float, int)
+    def start(self, start, end, stepsPerDataPoint):
+        if self.angle is None:
+            return
         
-        # TODO: implement method to handle measurement from start to finish
-        # method should report progress
-        # method should be able to be stopped, therefore it should check event loop for requests during execution
-        while self.running:
-            # self.progress_s.emit(0.5)
-            # loop =QEventLoop()
-            # loop.exec()
+        distance = end - self.angle
+        assert distance > 0 #assert for now
 
-            self._lockin.prepare()
+        self._lockin.prepare()
+        
+        self.dataproc.data_processing.create_new_file()
+        
+        self.running = True
 
-            #TODO read from gui
-            stepsPerDataPoint = 32 
-            end = None 
-            
-            distance = end - self.position
-            assert distance > 0 #assert for now
+        elem = self._elem   
+        steps = elem.angleToSteps(distance)
+        last_step = steps % stepsPerDataPoint
+        data_points = steps // stepsPerDataPoint
+
+        print(f"dist: {distance} \nsteps: {steps} \nvalues: {data_points}")
+
+        d = elem.stepsToAngle(stepsPerDataPoint)
+
+        measured_value = self._lockin.precitaj_hodnotu()
+        self.sendMeasurement(self.angle, measured_value)
+
+        for i in range(data_points):
+            if self.running == False:
+                break
+            print(f"iter: {i}")
                 
-            steps = self._elem.vlnaNaKroky(distance)
-            last_step = steps % stepsPerDataPoint
-            data_points = steps // stepsPerDataPoint + (1 if last_step != 0 else 0)
-
-            print(f"dist: {distance} \nsteps: {steps} \nvalues: {data_points}")
-
-            for i in range(data_points):
-                print(f"iter: {i}")
-                
-                duration = self._motor.moveForward(stepsPerDataPoint)
-                time.sleep(duration)
-                
-                measured_value = self._lockin.precitaj_hodnotu()
-                cur_pos = self.position + self._elem.krokyNaVlna((i+1)*stepsPerDataPoint)
-                print(f"pos: {cur_pos:.3f} measurement: {measured_value}")
-
-            self.position = cur_pos 
-    
-    def moveReverse(self, steps):
-        if not self.running:
-            self._motor.moveReverse(steps)
+            duration = self._motor.moveForward(stepsPerDataPoint)
+            time.sleep(duration)
+            self.angle += d
             
-
-    def moveForward(self, steps):
-        if not self.running:
-            self._motor.moveForward(steps)
+            measured_value = self._lockin.precitaj_hodnotu()
+            self.sendMeasurement(self.angle, measured_value)
             
+            print(f"pos: {self.angle:.3f} measurement: {measured_value}")
 
+        if last_step > 0 and self.running:
+            duration = self._motor.moveForward(last_step)
+            time.sleep(duration)
+            self.angle += elem.stepsToAngle(last_step)
+            
+            measured_value = self._lockin.precitaj_hodnotu()
+            self.sendMeasurement(self.angle, measured_value)
+            
+            print(f"pos: {self.angle:.3f} measurement: {measured_value}")
+            
+        self.running = False
+
+##    @QtCore.Slot()
+    def stop(self):
+        self.running = False
+
+    @QtCore.Slot(float)
+    def moveTest(self, steps=20):
+        print(f'move je uspesny, pocet krokov: {steps}')
+        self._motor.moveForward(int(steps))
+
+    @QtCore.Slot(float)
     def moveToPos(self, stop):
-        if self.position is None or self.position == stop or not self._elem.canMove(stop) or self.running:
+        if self.angle is None or self.angle == stop or not self._elem.canMove(stop) or self.running:
             return
 
-        distance = stop - self.position
-        steps = self._elem.vlnaNaKroky(abs(distance))
+        distance = stop - self.angle
+        steps = self._elem.angleToSteps(abs(distance))
         print("number of steps", steps)
         ack = input("write 'go' if agree:")
         if ack == "go":
-            self.position = stop
+            self.angle = stop
             if distance > 0:
                 self.moveForward(steps)
             else:
                 self.moveReverse(steps)
-                
+
+    @QtCore.Slot(int)
+    def moveForward(self, steps):
+        print(f'moveF je uspesny, pocet krokov: {steps}')
+        self._motor.moveForward(steps)
+
+    @QtCore.Slot(int)
+    def moveReverse(self, steps):
+        print(f'moveR je uspesny, pocet krokov: {steps}')
+        self._motor.moveReverse(steps)
+        
+    @QtCore.Slot(float)
+    def initialization(self, pos):
+        self.angle = pos
     
-    def calibration(self):
-        if not self.running:
-            start = float(input('zadaj startovaciu polohu: '))
-            steps = 0
-            while True:
-                userInput = int(input('pocet krokov dopredu: '))
-                if vstup == 0:
-                    break
-                steps += userInput
-                self.moveForward(userInput)
-
-            if steps <= 0:
-                return
-
-            self.position = end = float(input('zadaj koncovu polohu: '))
-
-            res = (end - start) / steps
-            self._elem.krok = res
-            
-                
-    def initialization(self, p):
-        self.position = p
-        
-
-    def save_calibration(self):
-        self._elem.save()
-        
-    @QtCore.Slot()
-    def stop(self):
-        # TODO: implement method to stop measurement. Checks if measurement is running and if so, stops it and emits signal that state changed
-        self.running = False
-
     def exit(self):
-        if self._motor is not None:
-            self._motor.motor.close()
-
-        if self._lockin is not None:
-            self._lockin.ser.close()
-            
         self._measurement_thread.quit()
         self._measurement_thread.wait()
