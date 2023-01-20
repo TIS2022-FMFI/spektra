a172454fc2f0543fe8e82d30bd2dbbb63d041b9c
Nesciuse <36797145+Nesciuse@users.noreply.github.com>
2023-01-20 13:19:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog, QHeaderVie
 from controllers.main_controller import MainController
 from view.view import View
 from settings import Settings
+from controllers.measurement_controller.data_processing_controller import DataProcessingController
 
 os.environ["QT_STYLE_OVERRIDE"] = "Fusion"
-
 
 
 class MainWindow(QMainWindow):
@@ -20,11 +20,15 @@ class MainWindow(QMainWindow):
         self._secret = random.random()
         self.view = View(self)
         self.controller = MainController(self._secret)
+        self.data_processing_controller = DataProcessingController(self.view)
         self._connect_view_controller()
         self.setWindowTitle(Settings.TITLE)
+        
         self.show()
 
-
+    def set_legend_item(self, q_line_edit, key):
+        print("sprava ", q_line_edit.text())
+        
     def _connect_view_controller(self):
         # connect the view with controllers
         self._connect_file_manager_controller()
@@ -68,29 +72,47 @@ class MainWindow(QMainWindow):
         pass
 
     def _connect_measurement_controller(self):
+        Widgets = self.view.widgets
+        de_cbox = Widgets.devices_controls_devices_selection_disperse_cbox
+        ms_cont = self.controller._measurement
+
+        ms_cont.set_dataproc_ref(self.data_processing_controller, self.view.widgets.graph_view)
+
+        de_cbox.addItem('ms732')
+        for i in range(5):
+            de_cbox.addItem(f'mriezka{i}')
+        
+        de_cbox.activated.connect(
+            lambda: ms_cont.disp_elem_change(de_cbox.currentText()))
+        ms_cont.disp_elem_change(de_cbox.currentText())
+
         self.controller._measurement.state_s.connect(lambda x: self.controller.logger.log(40, x, True))
-        self.view.widgets.comparative_file_unload_btn.clicked.connect(self.test)
+        Widgets.comparative_file_unload_btn.clicked.connect(self.test)
+
+        # moveForward/moveReverse
+        noStepsValue = lambda : Widgets.devices_controls_engine_positioning_step_sbox.value()
+        Widgets.devices_controls_engine_positioning_right_btn.clicked.connect(
+            lambda:self.controller.move_reverse(noStepsValue()))
+        Widgets.devices_controls_engine_positioning_left_btn.clicked.connect(
+            lambda:self.controller.move_forward(noStepsValue()))
         
         # moveToPosition
-        gotoValue = lambda: self.view.widgets.devices_controls_goto_sbox.value()
-        self.view.widgets.devices_controls_goto_btn.clicked.connect(lambda: self.controller.moveToPos(gotoValue))
+        gotoValue = lambda : Widgets.devices_controls_goto_sbox.value()
+        Widgets.devices_controls_goto_btn.clicked.connect(
+            lambda:self.controller.go_to_pos(gotoValue()))
 
+        # init position
+        initValue = lambda : Widgets.doubleSpinBox.value()
+        Widgets.devices_controls_calibration_btn.clicked.connect(
+            lambda:self.controller.initialization(initValue()))
 
-        numberOfSteps = lambda:  self.view.widgets.devices_controls_engine_positioning_step_sbox.value()
+        # meranie !!pozor start/end naopak v main_ui.py!!
+        endValue = lambda : Widgets.measurement_config_menu_start_sbox.value()
+        startValue = lambda : Widgets.measurement_config_menu_end_sbox.value()
+        motorStepValue = lambda : Widgets.measurement_motor_step.value()
 
-        # moveForward
-        self.view.widgets.devices_controls_engine_positioning_left_btn.clicked.connect(
-            self.controller.moveForvard(numberOfSteps))
-        
-        # moverReverse
-        self.view.widgets.devices_controls_engine_positioning_right_btn.clicked.connect(
-            self.controller.moveReverse(numberOfSteps))
-
-        # initialization
-        init_position = self.view.widgets.doubleSpinBox.value()
-        self.view.widgets.devices_controls_calibration_btn.clicked.connect(
-            self.controller.initialization(init_position))
-        
+        Widgets.action_play.triggered.connect(
+            lambda:self.controller.start_measurement(startValue(), endValue(), motorStepValue()))
 
     def test(self):
         print("test() thread id: ")
