769203cb4fe517ec8516b604b68f910ff3f89aca
Nesciuse <hornaksebastian@gmail.com>
2023-01-29 04:42:31 +0100

    -refactoring -changed connection and forward prep logic

--- models/motor/Motor.py
+++ models/motor/Motor.py
@@ -1,37 +1,36 @@
-import time
-import serial
- 
+from time import sleep
+from serial import Serial, EIGHTBITS, STOPBITS_ONE, PARITY_NONE
+
+
 class Motor:
     STEP_DELAY = 0.16
-    MOVING_CONSTANT = 40
+    FORWARD_PREPARATION_STEPS = 40
     MIN_STEPS_FOR_ACCELERATED_MOVEMENT = 201
 
-    def __init__(self, portName, delay=0.05):
+    def __init__(self, delay=0.05):
         self.delay = delay
         self.motor = None
         self.connected = False
 
-        try:
-            self.motor = serial.Serial(portName, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=0.5)
-            self.connected = True
-        except Exception as e:
-            print(e)
-            print('MOTOR NOT CONNECTED')
+    def connect(self, comport):
+        self.motor = Serial(comport, 9600, EIGHTBITS, PARITY_NONE, STOPBITS_ONE, timeout=0.5)
+        self.connected = True
 
-    def moveForward(self, steps):
+    def move_forward(self, steps):
         return self.move(steps, "F")
 
-    def moveReverse(self, steps):
-        added_steps = self.MOVING_CONSTANT if steps > 200 else 0
-        time.sleep(self.move(steps + added_steps, "R"))
-        return self.move(added_steps, "F")
-        
+    def move_reverse(self, steps, prepare_forward_movement=True):
+        if prepare_forward_movement:
+            sleep(self.move(steps + self.FORWARD_PREPARATION_STEPS, "R"))
+            return self.move(self.FORWARD_PREPARATION_STEPS, "F")
+        return self.move(steps, "R")
+
     def move(self, steps, direction):
         command = f"!{steps:04d}{direction}"
- 
+
         for char in command:
             log = self.motor.write(char.encode())
-            time.sleep(self.delay)
+            sleep(self.delay)
             print(log, end=' ')
 
         if steps >= self.MIN_STEPS_FOR_ACCELERATED_MOVEMENT:
@@ -40,4 +39,4 @@ class Motor:
             moving_time = steps * self.STEP_DELAY
 
         return moving_time
-    
+
